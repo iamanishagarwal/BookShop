10ea2837e1fec4a86cb71b2dff967d04f0185a5d
Anish <iamanishagarwal@gmail.com>
2019-09-14 21:26:00 +0530

    Implemented the googleapis in node.js server for searching book by term and id

--- server/routes/book.js
+++ server/routes/book.js
@@ -1,4 +1,43 @@
 const express = require("express");
+const fetch = require("node-fetch");
+const keys = require("../config/keys");
 const router = express.Router();
+
+const baseUrl = "https://www.googleapis.com/books/v1/volumes";
+
+router.get("/search/term", (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  fetch(
+    `${baseUrl}?q=${searchTerm}&key=${keys.googleApiKey}&startIndex=0&maxResults=6&printType=books`
+  )
+    .then(response => response.json())
+    .then(result => {
+      arr = result.items.map(item => {
+        return {
+          id: item.id,
+          title: item.volumeInfo.title,
+          subtitle: item.volumeInfo.subtitle,
+          author: item.volumeInfo.authors.toString(),
+          img: item.volumeInfo.imageLinks.smallThumbnail
+        };
+      });
+      res.send(arr);
+    })
+    .catch(err => {
+      console.log(err);
+      res.send("Error");
+    });
+});
+
+router.get("/search/id", (req, res) => {
+  const id = req.query.id;
+  fetch(`${baseUrl}/${id}?key=${keys.googleApiKey}`)
+    .then(response => response.json())
+    .then(result => res.send(result))
+    .catch(err => {
+      console.log(err);
+      res.send("Error");
+    });
+});
 
 module.exports = router;
